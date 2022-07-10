8765e3c42cedf30af65e7a9313cf228267427ec3
suweishuai <suwsl@foxmail.com>
2022-07-10 12:14:55 +0800

    add zicsr support

--- bsp/qemu-virt32-riscv/rtconfig.py
+++ bsp/qemu-virt32-riscv/rtconfig.py
@@ -38,7 +38,7 @@ if PLATFORM == 'gcc':
     OBJDUMP = PREFIX + 'objdump'
     OBJCPY  = PREFIX + 'objcopy'
 
-    DEVICE  = ' -mcmodel=medany -march=rv32ima -mabi=ilp32'
+    DEVICE  = ' -mcmodel=medany -march=rv32ima_zicsr -mabi=ilp32'
     CFLAGS  = DEVICE + ' -fvar-tracking -ffreestanding -fno-common -ffunction-sections -fdata-sections -fstrict-volatile-bitfields '
     AFLAGS  = ' -c' + DEVICE + ' -x assembler-with-cpp'
     LFLAGS  = DEVICE + ' -nostartfiles -Wl,--gc-sections,-Map=rtthread.map,-cref,-u,_start -T link.lds -lc -lm '
